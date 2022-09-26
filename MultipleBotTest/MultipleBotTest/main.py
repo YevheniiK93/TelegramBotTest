e7a79c399debcdd854d7e868355a9b7d9b5157f4
YevheniiK93 <kostiuk.yevhenii.py@gmail.com>
2022-09-26 20:37:16 +0300

    TelegramBotTest

---
+++ MultipleBotTest/MultipleBotTest/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import telebot
+import os
+
+app = Flask(__name__)
+TOKEN = os.environ.get("TOKEN")  #os - server request module. environ - dict
+bot = telebot.TeleBot(TOKEN)  #connect to telegram
+
+
+@bot.message_handler(commands=["start"])
+def message_start(message):
+    bot.send_message(message.chat.id, "Hello, user!\nPrint number in range 2-10")
+
+
+@bot.message_handler(func=lambda x: int(x) in range(2, 11))
+def message_table(message):
+    for i in range(1, 10):
+        res = int(message) * i
+        bot.send_message(message.chat.id, f"{res}")
+
+
+@bot.message_handler(func=lambda x: x.text.lower().startswith("python"))
+def message_text(message):
+
+    bot.send_message(message.chat.id, "Python")
+
+
+@app.route("/" + TOKEN, methods=["POST"])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot", 200
+
+@app.route("/")
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://multipletestbot.herokuapp.com/" + TOKEN)  #url - adress of app(Heroku)
+    return "Python Telegram Bot", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
