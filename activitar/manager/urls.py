632e7d0ea4c2ee7b8601d48c60bbb616f600503e
YevheniiK93 <110722667+YevheniiK93@users.noreply.github.com>
2022-10-29 14:56:54 +0300

    Delete activitar directory

--- activitar/manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import submit_list, submit_update
-
-app_name = 'manager'
-
-urlpatterns = [
-    path('submits/', submit_list, name='submits'),
-    path('submits/update/<int:pk>', submit_update, name='submit_close'),
-]
